4d56109a207d7fda95dc9abaafde90f6600424c3
Bhav Beri <bhavberi@gmail.com>
2023-11-26 15:58:09 +0000

    Updated

---
+++ precache-manifest.dfce76169c17d9049b8e10de084debaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636602dc4ecc1e65dcf9ab53a2df3cd5",
+    "url": "/Neuroeconomics-Experiment/index.html"
+  },
+  {
+    "revision": "a83a4f5e97106f6fd86f",
+    "url": "/Neuroeconomics-Experiment/static/css/main.72df8cdf.chunk.css"
+  },
+  {
+    "revision": "78eefe828be5b05fb6f0",
+    "url": "/Neuroeconomics-Experiment/static/js/2.fb827e7c.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Neuroeconomics-Experiment/static/js/2.fb827e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83a4f5e97106f6fd86f",
+    "url": "/Neuroeconomics-Experiment/static/js/main.f496f471.chunk.js"
+  },
+  {
+    "revision": "5a3b582f95ff98c04138",
+    "url": "/Neuroeconomics-Experiment/static/js/runtime-main.b8de3acc.js"
+  },
+  {
+    "revision": "f7ea6e19becd0efb44ed1ca61ec4ad2e",
+    "url": "/Neuroeconomics-Experiment/static/media/failure.f7ea6e19.svg"
+  },
+  {
+    "revision": "6e5e5bc619c7da43ae71183cb674d258",
+    "url": "/Neuroeconomics-Experiment/static/media/success.6e5e5bc6.svg"
+  }
+]);
